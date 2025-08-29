556be3160b693969f60b8318c1d489e62d4dff1b
Sean DMR <sean.moranrichards@shopify.com>
2025-08-29 11:29:37 +0100

    Update default worktree path building to include repo owner
    if available

--- flake.nix
+++ flake.nix
@@ -19,7 +19,7 @@
       {
         packages.default = pkgs.stdenv.mkDerivation {
           pname = "gwt";
-          version = "0.1.0";
+          version = "0.2.0";
           src = ./.;
           dontBuild = true;
           installPhase = ''
